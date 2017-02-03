9bd1c5ed1b023b3d5b17c21cdac56bdb826f4053
Tom Hinton <tom.hinton@cse.org.uk>
2017-02-03 16:08:13 +0000

    try again

--- bolt.r.cse.org.uk.zsh
+++ bolt.r.cse.org.uk.zsh
@@ -5,7 +5,7 @@ if [[ -z "$TMUX" ]] && [[ -n "$SSH_CONNECTION" ]]; then
         while read -r session; do
             tmux kill-session -t "$session"
         done
-    session=$(tmux-list-session -a -F '#{session_id}' | head -n 1)
+    session=$(tmux-list-sessions -a -F '#{session_id}' | head -n 1)
     if [[ -z $session ]]; then
         exec tmux new-session main
     fi
