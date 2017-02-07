d6a11a0aa496980452c0f16d35ed656d3670ff01
Tom Hinton <tom.hinton@cse.org.uk>
2017-02-07 14:55:16 +0000

    fix git config, update history

--- git-config.sh
+++ git-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 git config filter.encrypt.clean "openssl aes-256-cbc -a -salt -pass file:$HOME/.zsh/history-password"
-git config filter.encrypt.smudge "openssl aes-256-cbc -d -a -salt -pass file:$HOME/.zsh/history-password$"
+git config filter.encrypt.smudge "openssl aes-256-cbc -d -a -salt -pass file:$HOME/.zsh/history-password"
 git config merge.histdb.driver "zsh -c \"source ~/.zsh/sqlite-history.zsh; _histdb_merge %O %A %B\""
 
 KEY="AAAAB3NzaC1yc2EAAAADAQABAAABAQC8B1DrrW4CIKEu+ZLkvk8C+1cdgMLHoDUpIzFaWhOiRimpsZ9KAX9a4LY0oCYziWCfxIKYILtz+Z93O/7zEyTQSa1Hu0ygh5t05qBY//o7NwhdvMikw5mGEgEcXgE8VC0tlfgZmz+c7n0sRwAQW2Gezqo9L5LhKaxtpNXWcYP/RYahR/RYqG7nK/cErurNG2qZznawWFnYivB+MSX2J3dl0dJXe8zsLmKens0wuDbsxoRJrvL24TlPktXWzGz324PEiCK5lvGdbl/s6wVAzJHHagqyschqGq7NXyI+jNUgJB8SxisHjYDq6LOJyc2i6VXZ39N1oqcDZ3I1QF78s0tD"
